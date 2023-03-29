60ad3b7ee4302d1fdeb77fd8f90424ca3d8585c9
Eric <ericnguyen23@gmail.com>
2023-03-29 15:18:02 -0700

    Added associations to specified tables

--- models/index.js
+++ models/index.js
@@ -1,16 +1,32 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: category_id,
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: product_id,
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: Product,
+  },
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+});
 
 module.exports = {
   Product,
